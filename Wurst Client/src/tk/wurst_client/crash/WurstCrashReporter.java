306d2dab45cd819b8a82a000e8ce25a660f3269c
Alexander01998 <contact.wurstimperium@gmail.com>
2015-11-19 17:39:19 +0100

    Finish Wurst Crash Reporter (broken)
    LWJGL makes it freeze. Might need to run inside the Minecraft window.

--- Wurst Client/src/tk/wurst_client/crash/WurstCrashReporter.java
+++ Wurst Client/src/tk/wurst_client/crash/WurstCrashReporter.java
@@ -13,21 +13,32 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 
 import net.minecraft.crash.CrashReport;
+import tk.wurst_client.WurstClient;
+import tk.wurst_client.utils.JsonUtils;
+import tk.wurst_client.utils.MiscUtils;
+
+import com.google.gson.JsonObject;
 
 public class WurstCrashReporter extends JFrame
 {
+	private static boolean crashed = false;
 	private CrashReport report;
-	private boolean doneReporting = false;
+	private JLabel lblPleaseReportThis;
+	private JButton btnDontSend;
+	private JButton btnSendReport;
+	private JScrollPane scrollPane;
+	private Component rigidArea;
 	
 	public WurstCrashReporter(CrashReport report)
 	{
-		setUndecorated(true);
+		crashed = true;
 		setTitle("Wurst has crashed!");
 		setAlwaysOnTop(true);
 		setSize(new Dimension(450, 300));
@@ -36,14 +47,14 @@ public class WurstCrashReporter extends JFrame
 		getContentPane().setLayout(
 			new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 		
-		JLabel lblPleaseReportThis =
+		lblPleaseReportThis =
 			new JLabel(
 				"<html>\r\n<body width=\"300px\">\r\n<br>\r\n<p><b>Wurst has crashed. Please report this crash so that we can fix the bug that caused it and prevent it from happening again in the future.</b>\r\n<br>\r\n<br>\r\n<hr>\r\n<br>\r\n<p>We have created an anonymous crash report that you can send to us by using the button below. Adding a comment describing what you were doing just before the crash occurred can help us to understand the problem.\r\n<br>\r\n<br>");
 		lblPleaseReportThis.setAlignmentX(Component.CENTER_ALIGNMENT);
 		lblPleaseReportThis.setHorizontalAlignment(SwingConstants.CENTER);
 		getContentPane().add(lblPleaseReportThis);
 		
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
 		scrollPane.setMaximumSize(new Dimension(400, 32767));
 		scrollPane.setBorder(new TitledBorder(UIManager
 			.getBorder("TitledBorder.border"), "Comment", TitledBorder.CENTER,
@@ -59,13 +70,21 @@ public class WurstCrashReporter extends JFrame
 		getContentPane().add(panel);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 		
-		JButton btnSendReport = new JButton("Send Report");
+		btnSendReport = new JButton("Send Report");
+		btnSendReport.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				sendReport(textArea.getText());
+			}
+		});
 		panel.add(btnSendReport);
 		
-		Component rigidArea = Box.createRigidArea(new Dimension(20, 20));
+		rigidArea = Box.createRigidArea(new Dimension(20, 20));
 		panel.add(rigidArea);
 		
-		JButton btnDontSend = new JButton("Don't Send");
+		btnDontSend = new JButton("Don't Send");
 		btnDontSend.addActionListener(new ActionListener()
 		{
 			@Override
@@ -73,7 +92,6 @@ public class WurstCrashReporter extends JFrame
 			{
 				try
 				{
-					doneReporting = true;
 					dispose();
 				}catch(Exception e1)
 				{
@@ -85,8 +103,39 @@ public class WurstCrashReporter extends JFrame
 		setVisible(true);
 	}
 	
-	public boolean isDoneReporting()
+	private void sendReport(String comment)
 	{
-		return doneReporting;
+		JsonObject jsonReport = new JsonObject();
+		jsonReport.addProperty("trace", report.getCauseStackTraceOrString());
+		jsonReport.addProperty("comment", comment);
+		jsonReport.addProperty("vWurst", WurstClient.VERSION);
+		jsonReport.addProperty("vMinecraft", "1.8");
+		jsonReport.addProperty("vJava", System.getProperty("java.version"));
+		jsonReport.addProperty("vJava", System.getProperty("os.name"));
+		String response;
+		try
+		{
+			response = MiscUtils.post(new URL("https://bugs.wurst-client.tk/report/"),
+				JsonUtils.gson.toJson(jsonReport), "application/json");
+			System.out.println(response);
+		}catch(Exception e)
+		{
+			response = "Crash report could not be sent. Please check your internet connection.";
+			e.printStackTrace();
+		}
+		
+		lblPleaseReportThis.setText(
+				"<html>\r\n<body width=\"300px\">\r\n<br>\r\n"
+				+ response
+				+ "\r\n<br>\r\n<br>");
+		scrollPane.setVisible(false);
+		btnSendReport.setVisible(false);
+		rigidArea.setVisible(false);
+		btnDontSend.setText("OK");
+	}
+
+	public static boolean isCrashed()
+	{
+		return crashed;
 	}
 }
